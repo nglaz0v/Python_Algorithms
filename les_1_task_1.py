388e887aef2e4851824ee8dfbc109627ffa35aac
Nikolay Glazov <glazov_n_o@mail.ru>
2022-03-20 19:00:17 +0300

    les_1_task_1 completed

--- les_1_task_1.py
+++ les_1_task_1.py
@@ -1,8 +1,13 @@
 # 1. Выполнить логические побитовые операции «И», «ИЛИ» и др. над числами 5 и
 # 6. Выполнить над числом 5 побитовый сдвиг вправо и влево на два знака.
 
-print(f"5 and 6 = {5 & 6}\t{bin(5)} and {bin(6)} = {bin(5 & 6)}")
-print(f"5  or 6 = {5 | 6}\t{bin(5)}  or {bin(6)} = {bin(5 | 6)}")
-print(f"5 xor 6 = {5 ^ 6}\t{bin(5)} xor {bin(6)} = {bin(5 ^ 6)}")
-print(f"5 shr 2 = {5 >> 2}\t{bin(5)} shr {bin(2)} = {bin(5 >> 2)}")
-print(f"5 shl 2 = {5 << 2}\t{bin(5)} shl {bin(2)} = {bin(5 << 2)}")
+op__5_and_6 = 5 & 6
+op__5_or_6 = 5 | 6
+op__5_xor_6 = 5 ^ 6
+op__5_shr_2 = 5 >> 2
+op__5_shl_2 = 5 << 2
+print(f"5 and 6 = {op__5_and_6} \t{bin(5)} and {bin(6)} = {bin(op__5_and_6)}")
+print(f"5  or 6 = {op__5_or_6} \t{bin(5)}  or {bin(6)} = {bin(op__5_or_6)}")
+print(f"5 xor 6 = {op__5_xor_6} \t{bin(5)} xor {bin(6)} = {bin(op__5_xor_6)}")
+print(f"5 shr 2 = {op__5_shr_2} \t{bin(5)} shr {bin(2)} = {bin(op__5_shr_2)}")
+print(f"5 shl 2 = {op__5_shl_2}\t{bin(5)} shl {bin(2)} = {bin(op__5_shl_2)}")
