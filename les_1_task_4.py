d7506473feabc85a57346d4d204fc1b386832b15
Nikolay Glazov <glazov_n_o@mail.ru>
2022-03-20 20:55:58 +0300

    les_1_task_4 completed

--- les_1_task_4.py
+++ les_1_task_4.py
@@ -1,8 +1,11 @@
 # Пользователь вводит две буквы. Определить, на каких местах алфавита они
 # стоят, и сколько между ними находится букв.
 
-smb1 = input("smb1: ").lower()
-smb2 = input("smb2: ").lower()
-print(f"#smb1: {ord(smb1) - (ord('a') - 1)}")
-print(f"#smb2: {ord(smb2) - (ord('a') - 1)}")
-print(f"between: {ord(smb2) - ord(smb1) - 1}")
+smb1 = input("smb1: ")[0].lower()
+smb2 = input("smb2: ")[0].lower()
+smb1_pos = ord(smb1) - (ord('a') - 1)
+smb2_pos = ord(smb2) - (ord('a') - 1)
+smb_dist = ord(smb2) - ord(smb1) - 1
+print(f"smb1 #: {smb1_pos}")
+print(f"smb2 #: {smb2_pos}")
+print(f"delta: {smb_dist}")
