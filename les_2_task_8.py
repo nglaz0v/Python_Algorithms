a3c9c06ea9871b7fad3946dafa23a39046b5a756
Nikolay Glazov <glazov_n_o@mail.ru>
2022-03-24 17:14:14 +0300

    les_2_task_8 completed

--- les_2_task_8.py
+++ les_2_task_8.py
@@ -5,4 +5,21 @@
 посчитать, задаются вводом с клавиатуры.
 """
 
-# ...
+
+def digit_count(num, dgt):
+    count = 0
+    while (num > 0):
+        digit = num % 10
+        if digit == dgt:
+            count += 1
+        num //= 10
+    return count
+
+
+n = int(input("N: "))
+digit = int(input("d: "))
+count = 0
+for i in range(n):
+    number = int(input(f"{i+1}: "))
+    count += digit_count(number, digit)
+print(f"{count=}")
