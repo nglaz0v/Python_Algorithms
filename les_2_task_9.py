597c5f104966331a59de77a2b29effc16dabeb8c
Nikolay Glazov <glazov_n_o@mail.ru>
2022-03-24 17:19:53 +0300

    les_2_task_9 completed

--- les_2_task_9.py
+++ les_2_task_9.py
@@ -4,4 +4,20 @@
 Вывести на экран это число и сумму его цифр.
 """
 
-# ...
+
+def digits_sum(number):
+    s = 0
+    while (number > 0):
+        digit = number % 10
+        s += digit
+        number //= 10
+    return s
+
+
+n = int(input("N: "))
+x = 0
+for i in range(n):
+    number = int(input(f"{i+1}: "))
+    if (digits_sum(number) > digits_sum(x)):
+        x = number
+print(f"{x=} ({digits_sum(x)})")
