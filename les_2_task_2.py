04a3edea75102ff0823c28e9ce796c45aa771369
Nikolay Glazov <glazov_n_o@mail.ru>
2022-03-24 13:32:08 +0300

    les_2_task_2 completed

--- les_2_task_2.py
+++ les_2_task_2.py
@@ -4,4 +4,15 @@
 введено число 34560, в нём 3 чётные цифры (4, 6 и 0) и 2 нечётные (3 и 5).
 """
 
-# ...
+number = int(input("N: "))
+even = 0
+odd = 0
+while (number > 0):
+    digit = number % 10
+    # print(f"{digit}")
+    if ((digit % 2) == 0):
+        even += 1
+    else:
+        odd += 1
+    number //= 10
+print(f"чётных: {even}, нечётных: {odd}")
