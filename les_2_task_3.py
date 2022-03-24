86ce3e11663d900c084779cad29ca67a1a2665ac
Nikolay Glazov <glazov_n_o@mail.ru>
2022-03-24 13:32:15 +0300

    les_2_task_3 completed

--- les_2_task_3.py
+++ les_2_task_3.py
@@ -4,4 +4,11 @@
 вывести на экран. Например, если введено число 3486, надо вывести 6843.
 """
 
-# ...
+number = int(input("N: "))
+result = 0
+while (number > 0):
+    digit = number % 10
+    result *= 10
+    result += digit
+    number //= 10
+print(f"X: {result}")
