71412fe0a356926903914e87eb33453755ee61e9
nglaz0v <glazov_n_o@mail.ru>
2022-04-02 19:06:16 +0300

    les_3_task_5 completed

--- les_3_task_5.py
+++ les_3_task_5.py
@@ -6,4 +6,15 @@
 отрицательный». Это два абсолютно разных значения.
 """
 
-# ...
+import random
+
+n = int(input("N: "))
+k = int(input("K: "))
+a = [random.randint(-k, k) for i in range(n)]
+print(a)
+
+max_neg_i, max_neg_v = 0, -k
+for i in range(len(a)):
+    if (a[i] < 0) and (a[i] > max_neg_v):
+        max_neg_i, max_neg_v = i, a[i]
+print(f"{max_neg_i}: {max_neg_v}")
