b169cbf5d5243add66fa016d66bf489016c55419
nglaz0v <glazov_n_o@mail.ru>
2022-04-02 19:06:24 +0300

    les_3_task_6 completed

--- les_3_task_6.py
+++ les_3_task_6.py
@@ -5,4 +5,25 @@
 включать.
 """
 
-# ...
+import random
+
+n = int(input("N: "))
+k = int(input("K: "))
+a = [random.randint(-k, k) for i in range(n)]
+print(a)
+
+min_i, max_i = 0, 0
+min_v, max_v = a[0], a[0]
+for i in range(len(a)):
+    if a[i] < min_v:
+        min_i, min_v = i, a[i]
+    if a[i] > max_v:
+        max_i, max_v = i, a[i]
+print(f"{min_i}: {min_v}; {max_i}: {max_v}")
+
+if (min_i > max_i):
+    min_i, max_i = max_i, min_i
+s = 0
+for i in range(min_i+1, max_i):
+    s += a[i]
+print(f"{s}")
