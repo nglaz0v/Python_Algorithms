dbdd7eaa6c5aeba534485baf9f70e722fe0587aa
nglaz0v <glazov_n_o@mail.ru>
2022-04-02 19:06:32 +0300

    les_3_task_7 completed

--- les_3_task_7.py
+++ les_3_task_7.py
@@ -4,4 +4,18 @@
 быть как равны между собой (оба минимальны), так и различаться.
 """
 
-# ...
+import random
+
+n = int(input("N: "))
+k = int(input("K: "))
+a = [random.randint(-k, k) for i in range(n)]
+print(a)
+
+min_i = [0, 0]
+min_v = [a[0], a[0]]
+q = 0
+for i in range(1, len(a)):
+    if a[i] < min_v[q]:
+        min_i[q], min_v[q] = i, a[i]
+        q = (q + 1) % 2
+print(f"{min_i}: {min_v}")
