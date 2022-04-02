dd43304b9d8035448bbfca6d23815015f22538d7
nglaz0v <glazov_n_o@mail.ru>
2022-04-02 19:06:07 +0300

    les_3_task_4 completed

--- les_3_task_4.py
+++ les_3_task_4.py
@@ -3,4 +3,20 @@
 Определить, какое число в массиве встречается чаще всего.
 """
 
-# ...
+import random
+
+n = int(input("N: "))
+k = int(input("K: "))
+a = [random.randint(0, k) for i in range(n)]
+print(a)
+
+stat = {k: 0 for k in set(a)}
+for x in a:
+    stat[x] += 1
+print(stat)
+
+max_v = -k-1
+for val in stat:
+    if stat[val] > max_v:
+        max_v = val
+print(max_v)
