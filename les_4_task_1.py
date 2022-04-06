56a171f5121bfecfe34b5bd06205c7d823ba5671
Nikolay Glazov <glazov_n_o@mail.ru>
2022-04-06 16:04:49 +0300

    les_4_task_1: added reverse_1(), reverse_2(), reverse_3()

--- les_4_task_1.py
+++ les_4_task_1.py
@@ -11,22 +11,55 @@ d. результаты анализа вставить в виде коммен
 e. написать общий вывод: какой из трёх вариантов лучше и почему.
 """
 
-# from les_2_task_2 import count_even_odd
-# from les_2_task_3 import int_reverse
-# from les_2_task_8 import count_digit
-# from les_2_task_9 import digits_sum
+
+def reverse_1(number):
+    """Инвертировать порядок цифр натурального числа (версия 1)"""
+    result = 0
+    while (number > 0):
+        digit = number % 10
+        result *= 10
+        result += digit
+        number //= 10
+    return result
 
 
-def task(n):
-    for i in range(n):
-        x = i**i
+def reverse_2(number):
+    """Инвертировать порядок цифр натурального числа (версия 2)"""
+    result = ""
+    for i in str(number):
+        result = i + result
+    return int(result)
 
 
-# python3 -m timeit -n 1000 -s "from les_4_task_1 import task" "task(100)"
-# 1000 loops, best of 5: 35.6 usec per loop
+def reverse_3(number):
+    """Инвертировать порядок цифр натурального числа (версия 3)"""
+    return int(str(number)[::-1])
+
+
+def test_task(n):
+    """Проверка: сравнение значений функций reverse() между собой"""
+    a = reverse_1(n)
+    b = reverse_2(n)
+    c = reverse_3(n)
+    print(f"n : {a} = {b} = {c}")
+    assert a == b == c
+
+
+# python3 -m timeit -n 1000 -s "from les_4_task_1 import reverse_1" "reverse_1(1234567890987654321)"
+# 1000 loops, best of 5: 2.87 usec per loop
+# python3 -m timeit -n 1000 -s "from les_4_task_1 import reverse_2" "reverse_2(1234567890987654321)"
+# 1000 loops, best of 5: 1.18 usec per loop
+# python3 -m timeit -n 1000 -s "from les_4_task_1 import reverse_3" "reverse_3(1234567890987654321)"
+# 1000 loops, best of 5: 414 nsec per loop
 
 
 if __name__ == "__main__":
-    import cProfile
-    cProfile.run("task(100)")
-    # 1    0.000    0.000    0.000    0.000 les_4_task_1.py:23(task)
+    test_task(1234567890987654321)
+
+#    import cProfile
+#    cProfile.run("reverse_1(1234567890987654321)")
+#        1    0.000    0.000    0.000    0.000 les_4_task_1.py:15(reverse_1)
+#    cProfile.run("reverse_2(1234567890987654321)")
+#        1    0.000    0.000    0.000    0.000 les_4_task_1.py:15(reverse_2)
+#    cProfile.run("reverse_3(1234567890987654321)")
+#        1    0.000    0.000    0.000    0.000 les_4_task_1.py:15(reverse_3)
