dc454497df6cdb6875991bf6c6fc5dcf5eaffd60
Nikolay Glazov <glazov_n_o@mail.ru>
2022-04-25 13:24:47 +0300

    les_7_task_3: 'below' and 'above' now are lists

--- les_7_task_3.py
+++ les_7_task_3.py
@@ -18,36 +18,42 @@ array = [i for i in range(size)]
 random.shuffle(array)
 print(array)
 
-below = {array[0]}  # "нижние" элементы (которые меньше медианы)
-above = {array[0]}  # "верхние" элементы (которые больше медианы)
-for i in range(1, len(array)):
+below = [min(array)]  # "нижние" элементы (которые меньше медианы)
+above = [max(array)]  # "верхние" элементы (которые больше медианы)
+for i in range(0, len(array)):
     # print(f"{array[i]=}: {below=} {above=}")
-    if (array[i] < max(below)):  # очередной элемент меньше максимума из "нижних"
-        below.add(array[i])  # добавить его к "нижним"
-    elif (array[i] > min(above)):  # очередной элемент больше минимума из "верхних"
-        above.add(array[i])  # добавить его к "верхним"
+    if (array[i] <= max(below)):  # очередной элемент меньше максимума из "нижних"
+        below.append(array[i])  # добавить его к "нижним"
+    elif (array[i] >= min(above)):  # очередной элемент больше минимума из "верхних"
+        above.append(array[i])  # добавить его к "верхним"
     else:
-        # добавить к обоим множествам
-        below.add(array[i])
-        above.add(array[i])
-    # количество "нижних" и "верхних" элементов не должно различаться более чем на 1
-    if (len(below) > (len(above)+1)):
+        # добавить чётные к "нижним", а нечётные - к "верхним"
+        # (распределятся сами в ходе балансировки)
+        if (i % 2 == 0):
+            below.append(array[i])
+        else:
+            above.append(array[i])
+    # количество "нижних" и "верхних" элементов должно быть сбалансировано
+    # (не должно различаться более чем на 1)
+    if (len(below) >= (len(above)+1)):
         # максимум из "нижних" отправить к "верхним"
-        above.add(max(below))
+        above.append(max(below))
         below.remove(max(below))
-    elif (len(above) > (len(below)+1)):
+    elif (len(above) >= (len(below)+1)):
         # минимум из "верхних" отправить к "нижним"
-        below.add(min(above))
+        below.append(min(above))
         above.remove(min(above))
-# print(sorted(below))
-# print(sorted(above))
+below.remove(min(array))  # удалить лишний элемент из списка "нижних"
+above.remove(max(array))  # удалить лишний элемент из списка "верхних"
+# print(sorted(below), len(below))
+# print(sorted(above), len(above))
+# print(sorted(array), len(array))
+# print(sorted(below+above), len(below+above))
 
 median = None
 if len(below) < len(above):
     median = min(above)  # минимум из "верхних"
 elif len(below) > len(above):
     median = max(below)  # максимум из "нижних"
-else:
-    median = (below & above).pop()  # пересечение "нижних" и "верхних" множеств
 assert median == sorted(array)[m]
 print(f"{median = }")
