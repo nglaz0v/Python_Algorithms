fe01f83e1677004736ae034cecee3f1c26376cbf
Nikolay Glazov <glazov_n_o@mail.ru>
2022-04-25 11:53:48 +0300

    les_7_task_2: added comments

--- les_7_task_2.py
+++ les_7_task_2.py
@@ -21,13 +21,13 @@ def merge_sort(a, desc=False):
     cmp = gt if desc else lt
     if len(a) < 2:
         return a
-    a_L = merge_sort(a[:len(a) // 2], desc)
+    a_L = merge_sort(a[:len(a) // 2], desc)  # отсортированна левая половина
     i_L = 0
-    a_R = merge_sort(a[len(a) // 2:], desc)
+    a_R = merge_sort(a[len(a) // 2:], desc)  # отсортированна правая половина
     i_R = 0
-    a_C = [0] * (len(a_L) + len(a_R))
+    a_C = [0] * (len(a_L) + len(a_R))  # результат слияния отсортированных половин
     i_C = 0
-    while i_L < len(a_L) and i_R < len(a_R):
+    while (i_L < len(a_L)) and (i_R < len(a_R)):
         if cmp(a_L[i_L], a_R[i_R]):
             a_C[i_C] = a_L[i_L]
             i_L += 1
@@ -35,21 +35,21 @@ def merge_sort(a, desc=False):
             a_C[i_C] = a_R[i_R]
             i_R += 1
         i_C += 1
-    while i_L < len(a_L):
+    while (i_L < len(a_L)):
         a_C[i_C] = a_L[i_L]
         i_L += 1
         i_C += 1
-    while i_R < len(a_R):
+    while (i_R < len(a_R)):
         a_C[i_C] = a_R[i_R]
         i_R += 1
         i_C += 1
     for i in range(len(a)):
-        a[i] = a_C[i]
+        a[i] = a_C[i]  # заменить исходный массив на отсортированный
     return a
 
 
 print(__doc__)
-size = 12
+size = int(input("N: "))
 array = [random.random()*50 for i in range(size)]
 print(', '.join(f'{x:.2f}' for x in array))
 merge_sort(array)
