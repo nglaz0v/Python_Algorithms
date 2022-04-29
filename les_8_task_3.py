099a384d3bfb29be62f3ae007375d60e1547bea3
Nikolay Glazov <glazov_n_o@mail.ru>
2022-04-29 16:13:26 +0300

    les_8: added dummy code

--- les_8_task_3.py
+++ les_8_task_3.py
@@ -9,5 +9,28 @@ b. генерация графа выполняется в отдельной ф
 число вершин.
 """
 
+from collections import namedtuple
+
 print(__doc__)
-# ...
+# списки смежности
+graph_2a = {
+        0: {1, 2},
+        1: {0, 2, 3},
+        2: {0, 1},
+        3: {1},
+}
+print(graph_2a)
+print(3 in graph_2a[1])
+print('*'*20)
+Vertex = namedtuple("Vertex", ["vertex", "edge"])
+graph_2b = []
+graph_2b.append([Vertex(1, 2), Vertex(2, 3)])
+graph_2b.append([Vertex(0, 2), Vertex(2, 2), Vertex(3, 1)])
+graph_2b.append([Vertex(0, 3), Vertex(1, 2)])
+graph_2b.append([Vertex(1, 1)])
+print(*graph_2b, sep='\n')
+for v in graph_2b[1]:
+    if v.vertex == 3:
+        print(True)
+# print(any([v.vertex == 3 for v in graph_2b[1]]))
+print('*'*20)
