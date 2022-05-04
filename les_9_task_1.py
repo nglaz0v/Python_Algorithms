d97b8ae2b2d9e3f9f18276c20c3c15b7c6597ded
Nikolay Glazov <glazov_n_o@mail.ru>
2022-05-04 12:09:54 +0300

    les_9_task_1 completed

--- les_9_task_1.py
+++ les_9_task_1.py
@@ -9,5 +9,26 @@
   другой из модуля hashlib) задача считается не решённой.
 """
 
+import hashlib
+
+
+def count_substrs(s: str) -> int:
+    """
+    Определить количество различных подстрок в заданной строке
+    """
+    s = s.lower()
+    n = len(s)
+    assert n > 0, "Строка не может быть пустой"
+    hashes = set()
+    for k in range(1, n):
+        for i in range(n - k + 1):
+            substr = s[i:i+k]
+            print(f"{k} {i}: {substr}")
+            hashsub = hashlib.sha1(substr.encode("utf-8")).hexdigest()
+            hashes.add(hashsub)
+    return len(hashes)
+
+
 print(__doc__)
-# ...
+s = input("Введите строку: ")
+print(f"Количество различных подстрок в данной строке: {count_substrs(s)}")
